82328c3bae74966e830a01e472a64d219b31112f
Ramesh <ramesheorann@gmail.com>
2025-07-02 13:39:49 +0530

    second commit

--- GitPushDemo/src/main/java/Demo/GitPushDemoApplication.java
+++ GitPushDemo/src/main/java/Demo/GitPushDemoApplication.java
@@ -9,6 +9,7 @@ public class GitPushDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GitPushDemoApplication.class, args);
 		System.out.println("Hello Git");
+		System.out.println("Hello sonu ");
 	}
 
 }
