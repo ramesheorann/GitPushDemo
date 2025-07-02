aed5551f1f064b720efcb12632a0ebf7d9d91e9d
Ramesh <ramesheorann@gmail.com>
2025-07-02 13:36:11 +0530

    First Commit

---
+++ GitPushDemo/src/main/java/Demo/GitPushDemoApplication.java
@@ -0,0 +1,14 @@
+package Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPushDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPushDemoApplication.class, args);
+		System.out.println("Hello Git");
+	}
+
+}
---
+++ GitPushDemo/src/test/java/Demo/GitPushDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPushDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
